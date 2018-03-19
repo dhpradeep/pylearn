2c29ed021018e3b4469664ed4317610e14ab62bb
dhpradeep <dhpradeep25@gmail.com>
2018-03-19 15:01:44 +0545

    Initial

---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+	consumer_key =          'xxxxxx',
+	consumer_secret =       'xxxxxx',
+	access_token =          'xxxxxx',
+	access_token_secret =   'xxxxxx',
+)
